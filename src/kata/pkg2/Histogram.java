8cfad6040df28ff5c32ebbb33542b25584c5ef87
iraidacorvo <iraidacorvo@10.230.169.139>
2015-10-06 09:47:50 +0100

    Segunda Version

--- src/kata/pkg2/Histogram.java
+++ src/kata/pkg2/Histogram.java
@@ -1,23 +1,23 @@
 package kata.pkg2;
 import java.util.HashMap;
 
-public class Histogram {
-    private final int[] vector;
-    
-    public Histogram(int[]vector){
-    this.vector=vector;
+public class Histogram <T> {
+    private final T[] vector;
+    public Histogram(T[]vector)
+    {
+        this.vector=vector;
     }
-    public int []getElvector(){
+    public T[] getVector(){
         return vector;
     }
-    public  HashMap<Integer,Integer> getHisto(){
-        HashMap<Integer,Integer> histo =new HashMap<>();
+    public  HashMap<T,Integer> getHisto(){
+        HashMap<T,Integer> histo =new HashMap<>();
         for(int i=0;i<vector.length;i++){
             if(!histo.containsKey(vector[i])){
                 histo.put(vector[i],0);
             }
-            histo.put(vector[i],histo.get(i))
-        }      
+            histo.put(vector[i],histo.get(vector[i])+1);
+        }  
+        return histo;
     }  
-    return histo;
 }
--- src/kata/pkg2/Kata2.java
+++ src/kata/pkg2/Kata2.java
@@ -4,13 +4,13 @@ import java.util.HashMap;
 public class Kata2 {
     
     public static void main(String[] args) {
-        int[]array={1,4,7,1,1,8,4,1,4,9};
+        Integer []array={1,4,7,1,1,8,4,1,4,9};
+        String [] array1 ={"Ana","Juan","Pedro","Ana","Juan"};
         
-        Histogram histo=new Histogram(array);
-        int [] array 
-        HashMap <Integer,Integer> histogram = histo.getHisto();
+        Histogram histo=new Histogram(array1);
+        HashMap <String,Integer> histogram = histo.getHisto();
        
-        for(Integer key:histogram.keySet()){
+        for(Object key:histogram.keySet()){
             System.out.println( key+":"+ histogram.get(key));
         }
     }
